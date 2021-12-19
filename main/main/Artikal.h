ebd275952a336a821a665f589fcc2d980694252e
Husein22 <84035161+Husein22@users.noreply.github.com>
2021-12-19 14:37:01 +0100

    Create Artikal.h

---
+++ main/main/Artikal.h
@@ -0,0 +1,31 @@
+#pragma once
+//Proizvodjac:  Model:    Godina proizvodnje : RAM(GB) : ROM(GB) : Kolièina : Cijena(KM) :
+#include <istream>
+#include <string>
+class Artikal
+{
+protected:
+	std::string proizvodjac,model;
+	int kolicina,godina_proiz;
+	float cijena;
+public:
+	static int stanje_kase;
+	Artikal();
+	Artikal(std::string a, std::string b, int c, int d, float e);
+	//Artikal(const Artikal& p);
+	void setProizvodjac();
+	void setModel();
+	void setKolicina();
+	void setKolicina();
+	void setGodinaProizvodnje();
+	void setCijena();
+	std::string getProizvodjac();
+	std::string getModel();
+	int getKolicina();
+	int getGodinaProizvodnje();
+	float getCijena();
+	friend std::istream& operator>>(std::istream& stream, Artikal& a);
+	friend std::ostream& operator<<(std::ostream& stream, Artikal& a);
+	~Artikal() = default;
+};
+
