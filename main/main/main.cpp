d97e21c6ef9c4da5c50898d6d54baec4deb48a0c
Husein22 <84035161+Husein22@users.noreply.github.com>
2021-12-19 17:01:35 +0100

    Update main.cpp

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include<vector>
+#include "Laptop.h"
+#include<iomanip>
 using namespace std;
+vector<Laptop>laptopi;
+
+
+void unosLaptopa() {
+	std::shared_ptr<Laptop>temp = std::make_shared<Laptop>();
+	cin >> *temp;
+	laptopi.push_back(*temp);
+	cout << "Uspjesno smjesten laptop u vektor\n";
+}
+void ispisLaptopa() {
+	cout<< "-----------------------------------------------------------------------------------------------------------------------------\n";
+	cout << std::left << std::setw(15) << "Proizvodjac:" << std::left << std::setw(10) << "Model: " << std::left << std::setw(10) << "Kolièina:" <<
+		std::left << std::setw(20) << "Godina proizvodnje: " << std::left << std::setw(15) << "Cijena(KM) :" << std::left << std::setw(10)
+		<< std::left << std::setw(10) << "CPU" << std::left << std::setw(10) << "GPU" << std::left << std::setw(22) << "Operativni Sistem" << std::left << std::setw(10) <<
+		"RAM(GB): " << std::left << std::setw(10) << "HDD||SDD: \n";
+	cout << "------------------------------------------------------------------------------------------------------------------------------\n";
+	for (auto& i : laptopi) {
+		cout << i;
+	}
+}
 int main()
 {
-
+	unosLaptopa();
+	ispisLaptopa();
 }
