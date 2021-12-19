cb157b54723dd1575cff36064e11c7c6ac33fcb0
Husein22 <84035161+Husein22@users.noreply.github.com>
2021-12-19 15:08:54 +0100

    Create Laptop.h

---
+++ main/main/Laptop.h
@@ -0,0 +1,36 @@
+#pragma once
+#include"Artikal.h"
+#include<iostream>
+enum OperativniSistem  { Windows=0 ,Linux };
+class Laptop:
+	public Artikal
+{
+private:
+	std::string cpu, gpu;
+	OperativniSistem opSis;
+	int memorija, hard_drive;
+public:
+	Laptop();
+	Laptop(std::string a, std::string b, std::string c, int d, int e);
+	//Laptop(const Laptop* p);
+	void setCPU();
+	void setGPU();
+	void setOpSis();
+	void setMemorija();
+	void setHardDrivee();
+	std::string getCPU();
+	std::string getGPU();
+	OperativniSistem getOpSis();
+	std::string getOpsSisString();
+	int getMemorija();
+	int getHardDrive();
+	friend std::istream& operator>>(std::istream& stream, Laptop& a);
+	friend std::ostream& operator<<(std::ostream& stream, Laptop& a);
+
+
+	~Laptop() = default;
+
+};
+
+
+
