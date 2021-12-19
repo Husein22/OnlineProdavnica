a55be1def6c28e0c90091c82901ea4eaa0a59aba
Husein22 <84035161+Husein22@users.noreply.github.com>
2021-12-19 17:01:28 +0100

    Update Laptop.cpp

--- main/main/Laptop.cpp
+++ main/main/Laptop.cpp
@@ -1,5 +1,7 @@
 #include "Laptop.h"
-
+#include<iostream>
+#include<fstream>
+#include<iomanip>
 Laptop::Laptop()
 {
 }
@@ -27,10 +29,10 @@ void Laptop::setOpSis()
     do {
         std::cout << "Unesite izbor: ";
         std::cin >> z;
-        if (z > 1 || z < 0) {
+        if (z > 2 || z < 0) {
             std::cout << "[GRESKA]Pogresan izbor\n";
         }
-    } while (z >= 0 || z < 2);
+    } while (z < 0 || z > 2);
     this->opSis = static_cast<OperativniSistem>(z);
 }
 
@@ -90,24 +92,62 @@ int Laptop::getHardDrive()
 
 std::istream& operator>>(std::istream& stream, Laptop& a)
 {
-    std::ostream upis("Laptop.txt";std::ios:app)
-    a.setProizvodjac();
-    a.setModel();
-    a.setGodinaProizvodnje();
-    a.setCijena();
-    a.setCPU();
-    a.setGPU();
-    a.setOpSis();
-    a.setMemorija();
-    a.setHardDrivee();
+    std::ifstream some("Laptop.txt");
+    std::string temp;
+    some >> temp;
+    if (some.eof()) {
+        some.close();
+        std::ofstream upi("Laptop.txt");
+        upi << "-----------------------------------------------------------------------------------------------------------------------------\n";
+        upi << std::left << std::setw(15) << "Proizvodjac:" << std::left << std::setw(10) << "Model: " << std::left << std::setw(10) << "Kolièina:" <<
+            std::left << std::setw(20) << "Godina proizvodnje: " << std::left << std::setw(15) << "Cijena(KM) :" << std::left << std::setw(10)
+            << std::left << std::setw(10) << "CPU" << std::left << std::setw(10) << "GPU" << std::left << std::setw(22) << "Operativni Sistem" << std::left << std::setw(10) <<
+            "RAM(GB): " << std::left << std::setw(10) << "HDD||SDD: \n";
+        upi << "------------------------------------------------------------------------------------------------------------------------------\n";
+        upi.close();
+    }
+    else some.close();
 
+    std::ofstream upis("Laptop.txt", std::ios::app);
+    if (upis.is_open()) {
+        
+            a.setProizvodjac();
+            upis << std::left << std::setw(15) << a.getProizvodjac();
+            a.setModel();
+            upis << std::left << std::setw(10) << a.getModel();
+            a.setKolicina();
+            upis << std::left << std::setw(10) << a.getKolicina();
+            a.setGodinaProizvodnje();
+            upis << std::left << std::setw(20) << a.getGodinaProizvodnje();
+            a.setCijena();
+            upis << std::left << std::setw(15) << a.getCijena();
+            a.setCPU();
+            upis << std::left << std::setw(10) << a.getCPU();
+            a.setGPU();
+            upis << std::left << std::setw(10) << a.getGPU();
+            a.setOpSis();
+            upis << std::left << std::setw(22) << a.getOpsSisString();
+            a.setMemorija();
+            upis << std::left << std::setw(10) << a.getMemorija();
+            a.setHardDrivee();
+            upis << std::left << std::setw(10) << a.getHardDrive();
+            upis << "\n";
+            std::cout << "[CESTITAMO]Uspjesno smjestene informacije u datoteku\n";
+        }
+    else {
+        std::cout << "[GRESKA]Datoteka nije otvorena\n";
+    }
+    upis.close();
     return stream;
 }
 
 std::ostream& operator<<(std::ostream& stream, Laptop& a)
 {
-
-
+   
+    stream<< std::left << std::setw(15) << a.getProizvodjac() << std::left << std::setw(10) << a.getModel() << std::left << std::setw(10) << a.getKolicina()<< 
+        std::left << std::setw(20) << a.getGodinaProizvodnje() << std::left << std::setw(15) << a.getCijena()<< std::left << std::setw(10) << a.getCPU()
+      << std::left << std::setw(10) << a.getGPU()<<
+        std::left << std::setw(22) << a.getOpsSisString()<< std::left << std::setw(10) << a.getMemorija()<< std::left << std::setw(10) << a.getHardDrive()<< "\n";
 
     return stream;
 }
