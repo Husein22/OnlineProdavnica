e5136f14b1207d820ed07ac6b5224b42665f4edf
Husein22 <84035161+Husein22@users.noreply.github.com>
2021-12-19 20:53:17 +0100

    Update main.cpp

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
 #include<vector>
-#include "Laptop.h"
 #include<iomanip>
+#include<fstream>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include "Laptop.h"
+
 using namespace std;
 vector<Laptop>laptopi;
 
+void smjestizaIspis(string n) {
+	string temp;
+	string ime = n+ ".txt";
+	vector<string>lap;
+	std::ifstream smjesti(ime);
+	if (smjesti.is_open()) {
+		getline(smjesti, temp);
+		getline(smjesti, temp);
+		getline(smjesti, temp);
+		while (true) {
+			getline(smjesti, temp);
+			if (smjesti.eof())break;
+			lap.emplace_back(temp);
+		}
+		smjesti.close();
+	}
+	else {
+		cout << "Neuspjelo otvaranje datoteke\n";
+	}
+	for (auto& i : lap) {
+		cout << i;
+		cout << endl;
+	}
+	
 
+
+
+}
 void unosLaptopa() {
 	std::shared_ptr<Laptop>temp = std::make_shared<Laptop>();
 	cin >> *temp;
@@ -19,6 +51,7 @@ void ispisLaptopa() {
 		<< std::left << std::setw(10) << "CPU" << std::left << std::setw(10) << "GPU" << std::left << std::setw(22) << "Operativni Sistem" << std::left << std::setw(10) <<
 		"RAM(GB): " << std::left << std::setw(10) << "HDD||SDD: \n";
 	cout << "------------------------------------------------------------------------------------------------------------------------------\n";
+	smjestizaIspis("Laptop");
 	for (auto& i : laptopi) {
 		cout << i;
 	}
