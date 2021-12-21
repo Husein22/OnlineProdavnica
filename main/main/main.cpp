371c319a330e727d5a0e3c7fba15e907e9b30544
Husein22 <84035161+Husein22@users.noreply.github.com>
2021-12-21 17:20:28 +0100

    Update main.cpp

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -5,10 +5,149 @@
 #include<string>
 #include<cstring>
 #include<cmath>
+#include <conio.h>
+#include <windows.h>
+
+
 #include "Laptop.h"
 
 using namespace std;
 vector<Laptop>laptopi;
+
+int error;
+struct adminP {
+	string adminUser;string adminPass;
+};
+struct kupacP {
+	string kupacUser;string kupacPass;
+};
+void login(string& admin, string& password);
+void kupacLogin(string& user, string& password);
+
+string unosPassworda(string password, int ch = 0) {
+	password.clear();
+	while (ch = _getch()) {
+		if (ch != 13) cout << "*";
+		password += ch;
+		if (ch == 13) break;
+	}
+	password.erase(password.length() - 1);
+	return password;}
+
+void fontsize() {
+	short font, x, y;
+	do {
+		error = 0;cout << "\n" << setw(70) << "Upisi velicinu fonta: ";
+		cin >> font;
+		if (font >= 25 || font <= 5){
+			cout << "\n" << setw(68) << "Pogresan unos, unesite velicinu izmejdu 5 i 25. \n";
+			error = 1;cin.clear();cin.ignore();}
+	} while (error == 1);
+	x = font;y = 2 * x;
+	HANDLE Hout = ::GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD fsize = { x,y };CONSOLE_FONT_INFOEX Font = { sizeof(Font) };
+	::GetCurrentConsoleFontEx(Hout, FALSE, &Font);
+	Font.dwFontSize = fsize;SetCurrentConsoleFontEx(Hout, 0, &Font);
+	cin.clear();system("cls");
+}
+
+void tema() {
+	int boja;
+	cout << "\n" << setw(72) << "1. Black and White theme " << endl;
+	cout << "\n" << setw(72) << "2. White and Black theme " << endl;
+	cout << "\n" << setw(56) << "0. Return" << endl;
+	do {
+		do {
+			error = 0;
+			cout << "\n" << setw(67) << "Unesite svoj izbor: ";
+			cin >> boja;
+			if (boja > 2 || boja < 1) {
+				cout << "\n" << setw(65) << "Unos nije validan." << endl;
+				error = 1;
+				cin.clear();
+				cin.ignore(80, '\n');}
+		} while (error == 1);
+		if (boja == 1) {
+			system("color 0F");system("cls");
+			return;}
+		else if (boja == 2) {
+			system("color F0");
+			system("cls");return;}
+	} while (boja != 0);
+	system("cls");
+	return;
+}
+
+void login(string& admin, string& password) {
+	bool valid = false;adminP temp;
+	vector <adminP> vektor;
+	ifstream adminFile("admin.txt");
+	if (adminFile.is_open()) {
+
+		while (adminFile >> temp.adminUser) {
+			adminFile >> temp.adminPass;
+			vektor.push_back(temp);
+			if (vektor.size() >= 200)
+				break;
+		}
+	}
+	adminFile.close();
+	for (int i = 0; i < vektor.size(); i++) {
+		if (admin == vektor[i].adminUser && password == vektor[i].adminPass) {
+			cout << "\n\n" << setw(75) << "Uspjesno logirani na admin acc." << endl;
+			valid = true;
+			//adminMenu();
+			break;
+			Sleep(2000);
+		}
+	}
+	if (!valid) {
+		cout << "\n\n" << setw(75) << "Username ili password nisu validni." << endl;
+		system("pause");
+		Sleep(2000);
+		system("CLS");
+	}
+
+}
+void dodajProfil() {
+
+	ofstream file;
+	file.open("kupac.txt", std::ios_base::app);
+	if (!file) {
+		cout << "\n" << setw(80) << "Nije moguce otvoriti datoteku!";
+		return;}
+	kupacP k;
+	cout << "\n" << setw(63) << "Username: ";cin >> k.kupacUser;
+	cout << "\n" << setw(63) << "Password: ";cin >> k.kupacPass;
+	file << " " << k.kupacUser << " ";
+	file << k.kupacPass << endl;
+	file.close();Sleep(1000);system("CLS");
+}
+void kupacLogin(string& user, string& password) {
+	bool valid = false;kupacP temp;
+	vector <kupacP> vektor;
+	ifstream kupacFile("kupac.txt");
+	if (kupacFile.is_open()) {
+		while (kupacFile >> temp.kupacUser) {
+			kupacFile >> temp.kupacPass;
+			vektor.push_back(temp);
+			if (vektor.size() >= 200)
+				break;}}
+	kupacFile.close();
+	for (int i = 0; i < vektor.size(); i++) {
+		if (user == vektor[i].kupacUser && password == vektor[i].kupacPass) {
+			cout << "\n\n" << setw(75) << "Uspjesno logirani na kupac acc." << endl;
+			//kupacMenu();
+			valid = true;break;}
+		else if (user == "Guest" && password == "guest") {
+			cout << "\n\n" << setw(78) << "Uspjesno logirani na guest acc." << endl;
+			valid = true;break;}}
+	if (!valid) {
+		cout << "\n\n" << setw(78) << "Username ili password nisu validni." << endl;
+		system("pause");Sleep(2000);system("CLS");
+	}}
+
+
 
 void smjestizaIspis(string n) {
 	string temp;
@@ -49,6 +188,7 @@ void osnova() {
 		"RAM(GB) " << std::left << std::setw(10) << "HDD||SDD \n";
 	cout << "------------------------------------------------------------------------------------------------------------------------------\n";
 }
+
 void ispisLaptopa() {
 	osnova();
 	smjestizaIspis("Laptop");
@@ -96,9 +236,107 @@ void sortiranjePoSekundarnoj(){
 
 int main()
 {
+
+	int izbor;
+	string user, password;
+
+	do {
+		cout << "\n\n" << setw(81) << "****************************************" << endl;
+		cout << setw(77) << " * >>>> PRIJAVA NA IT SHOP <<<< *" << endl;
+		cout << setw(81) << "****************************************" << endl;
+
+		cin.clear();
+		cout << "\n" << setw(79) << "Odaberite neku od ponudjenih opcija :";
+		cout << "\n" << setw(63) << "1. Admin login: ";
+		cout << "\n" << setw(63) << "2. Kupac login: ";
+		cout << "\n" << setw(75) << "3. Promjeni velicinu fonta: ";
+		cout << "\n" << setw(65) << "4. Promjena teme: ";
+		cout << "\n" << setw(57) << "0. Kraj: \n";
+
+		do {
+			error = 0;
+			cout << "\n" << setw(69) << "Upisite svoj odabir : ";
+			cin >> izbor;
+			if (izbor > 4 || izbor < 0) {
+				cout << "\n" << setw(79) << "[POGRESAN IZBOR] Pokusajte ponovo: \n";
+				error = 1;
+				cin.clear();
+				cin.ignore();
+			}
+		} while (error == 1);
+		system("cls");
+		int z = 0;
+		switch (izbor) {
+		case 1: {
+			do {
+				cout << "Unesite sifru za ulazak u admin menu\n";
+				cin >> z;
+			} while (z != 123);
+			cout << "\n" << setw(60) << "Username: ";
+			cin >> user;
+			cout << "\n" << setw(57) << "Sifra: ";
+			password = unosPassworda(password);
+			login(user, password);
+		} break;
+		case 2: {
+			int n;
+			cout << "\n" << setw(79) << "Odaberite neku od ponudjenih opcija :";
+			cout << "\n" << setw(62) << "1. Kupac login ";
+			cout << "\n" << setw(70) << "2. Kupac registracija: ";
+			cout << "\n" << setw(70) << "0. Vrati se glavnu menu";
+
+			do {
+				error = 0;
+				cout << "\n" << setw(68) << "Upisi svoj izbor: ";
+				cin >> n;
+				if (n < 0 || n>2) {
+					cout << setw(65) << "Unesite validan unos: " << endl;
+					error = 1;
+					cin.clear();
+					cin.ignore();
+				}
+			} while (error == 1);
+
+			switch (n) {
+			case 1: {
+				cout << "\n" << setw(75) << "Molim vas unesite kupac username: ";
+				cin >> user;
+				cout << "\n" << setw(62) << "Sifra: ";
+				password = unosPassworda(password);
+				kupacLogin(user, password);
+				break;
+			}
+			case 2: {
+				dodajProfil();
+			}break;
+			case 0: {
+				system("CLS");
+			} break;
+			default: cout << setw(65) << "[GRESKA] -> Unos nije validan!" << endl;
+			}
+		}	break;
+		case 3: {
+			fontsize();
+		}break;
+		case 4: {
+			tema();
+			break;
+		}
+		case 0: {
+			system("pause");
+			return 0;
+		}
+		default: {
+			cout << "\n\n" << setw(65) << "[GRESKA] -> Unos nije validan!" << endl;
+			cout << "\n\n" << setw(62) << "Pokusajte ponovo sa 1,2,3,4,5, ili 0!" << endl;
+			system("pause");
+		}
+		}
+
+	} while (izbor != 0);
 	sortiranjePoSekundarnoj();
 	unosLaptopa();
 	pretragaPoProizziMod();
 	unosLaptopa();
-	//ispisLaptopa();
+	ispisLaptopa();
 }
