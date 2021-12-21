416dc722de4379c97bbe8e08f290434397220a6c
Husein22 <84035161+Husein22@users.noreply.github.com>
2021-12-21 13:42:51 +0100

    Update main.cpp

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -34,42 +34,71 @@ void smjestizaIspis(string n) {
 		cout << endl;
 	}
 }
+
 void unosLaptopa() {
 	std::shared_ptr<Laptop>temp = std::make_shared<Laptop>();
 	cin >> *temp;
 	//laptopi.push_back(*temp);
 	cout << "Uspjesno smjesten laptop u datoteku\n";
 }
-void ispisLaptopa() {
-	cout<< "-----------------------------------------------------------------------------------------------------------------------------\n";
-	cout << std::left << std::setw(15) << "Proizvodjac:" << std::left << std::setw(10) << "Model: " << std::left << std::setw(10) << "Kolièina:" <<
-		std::left << std::setw(20) << "Godina proizvodnje: " << std::left << std::setw(15) << "Cijena(KM) :" << std::left << std::setw(10)
+void osnova() {
+	cout << "-----------------------------------------------------------------------------------------------------------------------------\n";
+	cout << std::left << std::setw(15) << "Proizvodjac " << std::left << std::setw(10) << "Model " << std::left << std::setw(10) << "Kolièina" <<
+		std::left << std::setw(20) << "Godina proizvodnje " << std::left << std::setw(15) << "Cijena(KM) " << std::left << std::setw(10)
 		<< std::left << std::setw(10) << "CPU" << std::left << std::setw(10) << "GPU" << std::left << std::setw(22) << "Operativni Sistem" << std::left << std::setw(10) <<
-		"RAM(GB): " << std::left << std::setw(10) << "HDD||SDD: \n";
+		"RAM(GB) " << std::left << std::setw(10) << "HDD||SDD \n";
 	cout << "------------------------------------------------------------------------------------------------------------------------------\n";
+}
+void ispisLaptopa() {
+	osnova();
 	smjestizaIspis("Laptop");
-	/*for (auto& i : laptopi) {
-		cout << i;
-	}*/
 }
-void pretragaPoProizz() {
+
+void pretragaPoProizziMod() {
 	std::shared_ptr<Laptop>temp = std::make_shared<Laptop>();
+	std::unique_ptr<string>p = std::make_unique<string>();
 	std::unique_ptr<string>l = std::make_unique<string>();
 	std::cout << "Unesite proizvodjaca za pretragu: ";
 	getline(cin, *l);
-	temp->pretragaPoProiz(*l);
+	std::cout << "Unesite model za pretragu: ";
+	getline(cin, *p);
+	temp->pretragaPoProizImodelu(*l, *p);
 }
-void pretragaPoModeluu() {
-	std::shared_ptr<Laptop>temp = std::make_shared<Laptop>();
-	std::unique_ptr<string>l = std::make_unique<string>();
-	std::cout << "Unesite proizvodjaca za pretragu: ";
-	getline(cin, *l);
-	temp->pretragaPoModelu(*l);
-}
+
+
+void sortiranjePoSekundarnoj(){
+		std::ifstream unos("Laptop.txt");
+		std::vector<std::string>nizl;std::vector<int>hdd;std::string temp;int pom;std::string po;
+		int a, b, c, x, p = 0;
+		if (unos.fail())std::cout << "Nemoguce otvaranje datoeke\n";
+		else {
+			getline(unos, temp);getline(unos, temp);getline(unos, temp);
+			while (true) {
+				getline(unos, temp);
+				if (unos.eof())break;
+				nizl.push_back(temp);
+				a = temp[122] - '0';b = temp[123] - '0';c = temp[124] - '0';
+				if (a > -1 && b > -1 && c > -1) x = a * 100 + b * 10 + c + 1;
+				else if (a > -1 && b > -1 && c < -1) x = a * 10 + b;
+				else if (a > -1 && b < -1 && c < -1) x = a;
+				hdd.push_back(x);
+			}
+			for (int i = 0;i < hdd.size();i++) {
+				for (int j = i;j < hdd.size();j++) {
+					if (hdd[j] > hdd[i]) {
+						std::swap(hdd[j], hdd[i]);
+						std::swap(nizl[j], nizl[i]);
+					}}}
+			osnova();
+			for (int i = 0;i < hdd.size();i++)std::cout << nizl[i] << "\n";
+			unos.close();
+		}}
+
 int main()
 {
-	pretragaPoModeluu();
-	pretragaPoProizz();
+	sortiranjePoSekundarnoj();
 	unosLaptopa();
-	ispisLaptopa();
+	pretragaPoProizziMod();
+	unosLaptopa();
+	//ispisLaptopa();
 }
