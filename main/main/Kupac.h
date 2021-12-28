eeceab013631dd9813c97eccc33f23923932466e
Husein22 <84035161+Husein22@users.noreply.github.com>
2021-12-28 16:19:07 +0100

    Update Kupac.h

--- main/main/Kupac.h
+++ main/main/Kupac.h
@@ -4,9 +4,22 @@
 class Kupac
 {
 private:
-	std::string username, pass;
+	std::string username, pass,ime,prezime,kartica;
 public:
 	Kupac();
+	Kupac(std::string a, std::string b, std::string c, std::string d, std::string e);
+	void setusername();
+	void setPass();
+	void setIme();
+	void setPrezime();
+	void setKartica();
+	std::string getUsername();
+	std::string getPass();
+	std::string getIme();
+	std::string getPrezime();
+	std::string getKartica();
+	friend std::istream& operator>>(std::istream& stream, Kupac& a);
+	friend std::ostream& operator<<(std::ostream& stream, Kupac& a);
 	std::string unosPassworda(std::string password, int ch);
 	bool login(std::string& admin, std::string& password);
 	void dodajProfil();
